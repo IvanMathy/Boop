2316daa41a0ab6639a7ccbc479dd74774d21436e
Ivan Mathy <IvanMathy@users.noreply.github.com>
2019-07-19 23:13:55 -0400

    Removed built Savannahkit to use Carthage instead

--- Boop/Boop/Controllers/MainViewController.swift
+++ Boop/Boop/Controllers/MainViewController.swift
@@ -19,7 +19,6 @@ class MainViewController: NSViewController {
         
         editorView.delegate = self
         editorView.theme = DefaultTheme()
-        editorView.allowsAutocomplete = false
         
         editorView.contentTextView.selectedTextAttributes = [.backgroundColor:NSColor(red:0.19, green:0.44, blue:0.71, alpha:1.0), .foregroundColor: NSColor.white]
     
@@ -28,6 +27,14 @@ class MainViewController: NSViewController {
 }
 
 extension MainViewController: SyntaxTextViewDelegate {
+    func didChangeText(_ syntaxTextView: SyntaxTextView) {
+        
+    }
+    
+    func didChangeSelectedRange(_ syntaxTextView: SyntaxTextView, selectedRange: NSRange) {
+        
+    }
+    
     func didChangeFont(_ font: Font) {
         //
     }
