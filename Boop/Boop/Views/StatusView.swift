b961e97d44d340ba3fd98d43f68c0d25f63fadff
Ivan Mathy <IvanMathy@users.noreply.github.com>
2019-07-21 22:37:00 -0400

    Fixed signing issues, added status queue

--- Boop/Boop/Views/StatusView.swift
+++ Boop/Boop/Views/StatusView.swift
@@ -18,11 +18,12 @@ enum Status {
 @IBDesignable
 class StatusView: NSView {
     
-    let transitionLength = 0.5
+    let transitionLength = 0.3$
+    let messageLength = 5.0
     
     @IBOutlet weak var textLabel: NSTextField!
     
-    let queue = [Status]()
+    var queue = [Status]()
     var running = false
     
     
@@ -37,11 +38,46 @@ class StatusView: NSView {
     }
 
     func setStatus(_ newStatus: Status) {
-        updateText(newStatus)
-        updateColor(newStatus)
+        
+        switch newStatus {
+        case .normal, .help(_):
+            // Skip the queue for those statuses
+            running = false
+            queue.removeAll()
+            self.updateText(newStatus)
+            self.updateColor(newStatus)
+        default:
+            queue.append(newStatus)
+            queueUpdated()
+        }
+        
+        
     }
     
-
+    func queueUpdated() {
+        guard !running else {
+            return
+        }
+        
+        guard !queue.isEmpty else {
+            running = false
+            self.updateText(.normal)
+            self.updateColor(.normal)
+            return
+        }
+        
+        running = true
+        
+        let next = queue.removeFirst()
+        
+        self.updateText(next)
+        self.updateColor(next)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + messageLength, execute: {
+            self.running = false
+            self.queueUpdated()
+        })
+    }
     
     fileprivate func updateText(_ newStatus: Status) {
         
