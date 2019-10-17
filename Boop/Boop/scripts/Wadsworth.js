05cce22589a18e2aa8f5ae8bc2ab24ee015a8ac7
Ivan Mathy <IvanMathy@users.noreply.github.com>
2019-10-16 21:06:49 -0400

    Create Wadsworth.js

---
+++ Boop/Boop/scripts/Wadsworth.js
@@ -0,0 +1,17 @@
+/**
+	{
+		"api":1,
+		"name":"Wadsworth Constant",
+		"description":"first 30% of your text.",
+		"author":"Ivan",
+		"icon":"broom",
+		"tags":"snap"
+	}
+**/
+
+
+function main(state) {
+	var all = state.text.split(" ") 
+	all.splice(0, Math.ceil(all.length * 0.3))
+	state.text = all.join(" ")
+}
