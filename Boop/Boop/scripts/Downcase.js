da648dc1a29c7d093b7404508d591ff4916c92a2
Daniel Inkpen <dan2552@gmail.com>
2020-06-16 16:56:39 +0100

    Adds upcase and downcase

---
+++ Boop/Boop/scripts/Downcase.js
@@ -0,0 +1,16 @@
+/**
+    {
+        "api":1,
+        "name":"Downcase",
+        "description":"Converts to lowercase",
+        "author":"Dan2552",
+        "icon":"metamorphose",
+        "tags":"downcase,lowercase"
+    }
+**/
+
+function main(input) {
+    
+    input.text = input.text.toLowerCase();
+    
+}
