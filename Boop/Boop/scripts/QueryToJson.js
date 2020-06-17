13afb478ed85f4292944cde2f36e01f39ba7cbcf
Ota Mares <ota@rebuy.com>
2020-06-17 11:43:12 +0200

    Add JSON to Query converter and vice versa

---
+++ Boop/Boop/scripts/QueryToJson.js
@@ -0,0 +1,42 @@
+/**
+	{
+		"api":1,
+		"name":"Query String to JSON",
+		"description":"Converts URL query string to JSON",
+		"author":"Ota Mares <ota@mares.one>",
+		"icon":"table",
+		"tags":"url,query,params,json,convert,decode"
+	}
+**/
+
+function convertToJson(urlParams) {
+
+    return urlParams
+        .replace(/\[\d?\]=/gi, '=')
+        .split('&')
+        .reduce((result, param) => {
+            var [key, value] = param.split('=');
+            value = decodeURIComponent(value || '');
+
+            if (!result.hasOwnProperty(key)) {
+                result[key] = value;
+
+                return result;
+            }
+
+            result[key] = [...[].concat(result[key]), value]
+
+            return result
+        }, {});
+}
+
+function main(input)
+{
+    try {
+        input.text = JSON.stringify(convertToJson(input.text));
+    } catch (error) {
+        input.postError("Unable to parse given string")
+        input.text = error.message
+    }
+
+}
