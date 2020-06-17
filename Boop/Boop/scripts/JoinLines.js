d452c1b12d256cece412599ec0d7bdc4e0fb5c2b
Valentin Huber <valentinchuber@gmail.com>
2020-06-17 16:29:07 +0200

    added joinlines scripts

---
+++ Boop/Boop/scripts/JoinLines.js
@@ -0,0 +1,14 @@
+/**
+	{
+		"api":1,
+		"name":"Join Lines",
+		"description":"Joins All Lines Without a Delimiter",
+		"author":"riesentoaster",
+		"icon":"table",
+		"tags":"join"
+	}
+**/
+
+function main(input) {
+	input.text = input.text.replace('\n', '');
+}
