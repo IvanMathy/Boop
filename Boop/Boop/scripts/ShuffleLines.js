87d6cbdaff646e0146270e0fce09e89cd6972faa
Ivan Mathy <IvanMathy@users.noreply.github.com>
2020-06-20 18:43:09 -0500

    Bad merge

--- Boop/Boop/scripts/ShuffleLines.js
+++ Boop/Boop/scripts/ShuffleLines.js
@@ -1,5 +1,4 @@
 /**
-<<<<<<< HEAD
 	{
 		"api":1,
 		"name":"Shuffle Lines",
@@ -23,28 +22,4 @@ function main(input) {
 	}
 	
 	input.text = lines.join('\n');
-=======
-{
-  "api": 1,
-  "name": "Shuffle lines",
-  "description": "Shuffles lines randomly",
-  "author": "Christian Petersen",
-  "icon": "unknown",
-  "tags": "shuffle,random,line"
-}
-**/
-function shuffleString(string) {
-  const chars = string.split("\n");
-
-  for (let i = chars.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [chars[i], chars[j]] = [chars[j], chars[i]];
-  }
-
-  return chars.join("\n");
-}
-
-function main(input) {
-  input.text = shuffleString(input.text);
->>>>>>> pr/39
 }
