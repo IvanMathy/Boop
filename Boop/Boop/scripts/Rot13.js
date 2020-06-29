0a6c73645664e65944cbb599c0c492e2a2b561d5
papatangosierra <pts@sockdolager.net>
2020-06-29 16:38:46 -0400

    Update Rot13.js
    make sure we're updating state.text instead of, as before, state.fullText

--- Boop/Boop/scripts/Rot13.js
+++ Boop/Boop/scripts/Rot13.js
@@ -12,7 +12,7 @@
 function main(state) {
 	let myText = state.text
 	// adapted from Sophie Alpert's solution: https://stackoverflow.com/questions/617647/where-is-my-implementation-of-rot13-in-javascript-going-wrong
-	state.fullText = myText.replace(/[a-z]/gi, function (c) {
+	state.text = myText.replace(/[a-z]/gi, function (c) {
  		return String.fromCharCode(	
  				(c <= "Z" ? 90 : 122) >= (c=c.charCodeAt(0)+13) ?  c :  c - 26
  			);
