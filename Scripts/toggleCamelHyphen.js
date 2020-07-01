b49348cd9329259ce6945cb3ef8cd354293bb1c8
codepo8 <chris.heilmann@gmail.com>
2020-07-01 19:55:44 +0200

    Added script to convert camelCase to camel-case and vice versa

---
+++ Scripts/toggleCamelHyphen.js
@@ -0,0 +1,25 @@
+/**
+{
+  "api": 1,
+  "name": "Toggle Camel and Hyphen",
+  "description": "Turns camelCase to camel-case and vice versa",
+  "author": "Christian Heilmann",
+  "icon": "camel",
+  "tags": "camelcase,hyphencase,syntax,codestandards"
+}
+**/
+
+const toggleCamelHyphen = (str) => {
+  if (str.indexOf('-') !== -1) {
+    return str.replace(/\W+(.)/g, (x, chr) => {
+      return chr.toUpperCase();
+    });
+  } else {
+    return str.replace(/\W+/g, '-')
+    .replace(/([a-z\d])([A-Z])/g, '$1-$2').toLowerCase();
+  }
+}
+
+function main(input) {
+  input.text = toggleCamelHyphen(input.text);
+}
