687048113b05981f51efa56450655d9fd0695ba2
Ivan Mathy <IvanMathy@users.noreply.github.com>
2020-07-05 09:53:54 -0500

    Added 4 new scripts

---
+++ Boop/Boop/scripts/Deburr.js
@@ -0,0 +1,18 @@
+/**
+	{
+		"api":1,
+		"name":"Deburr",
+		"description":"Converts your text to basic latin characters",
+		"author":"Ivan",
+		"icon":"metamorphose",
+		"tags":"burr,special,characters,function,lodash"
+	}
+**/
+
+const { deburr } = require('@boop/lodash.boop')
+
+function main(input) {
+	
+    input.text = deburr(input.text)
+	
+}
---
+++ Boop/Boop/scripts/KebabCase.js
@@ -0,0 +1,18 @@
+/**
+	{
+		"api":1,
+		"name":"Kebab Case",
+		"description":"converts-your-text-to-kebab-case",
+		"author":"Ivan",
+		"icon":"metamorphose",
+		"tags":"kebab,case,function,lodash"
+	}
+**/
+
+const { kebabCase } = require('@boop/lodash.boop')
+
+function main(input) {
+	
+    input.text = kebabCase(input.text)
+	
+}
