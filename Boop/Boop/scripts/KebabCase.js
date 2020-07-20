55b1902308d758b171d93cab5c78b3a49cb65c35
Ivan Mathy <IvanMathy@users.noreply.github.com>
2020-07-20 08:31:23 -0500

    Fixes

--- Boop/Boop/scripts/KebabCase.js
+++ Boop/Boop/scripts/KebabCase.js
@@ -4,7 +4,7 @@
 		"name":"Kebab Case",
 		"description":"converts-your-text-to-kebab-case.",
 		"author":"Ivan",
-		"icon":"metamorphose",
+		"icon":"kebab",
 		"tags":"kebab,case,function,lodash"
 	}
 **/
