0e7864b30e83d69283e081d302b7bb66aeff4cfb
xshoji <xshoji@users.noreply.github.com>
2020-10-25 04:16:04 +0900

    Add comments.

--- Scripts/convertToMarkdownTable.js
+++ Scripts/convertToMarkdownTable.js
@@ -24,6 +24,7 @@ function convertToPrettyMarkdownTableFormat(input) {
     let length = 0;
     for (let i = 0; i < character.length; i++) {
       const c = character.charCodeAt(i);
+      // Multibyte handling
       (c >= 0x0 && c < 0x81) || (c === 0xf8f0) || (c >= 0xff61 && c < 0xffa0) || (c >= 0xf8f1 && c < 0xf8f4) ? length += 1 : length += 2;
     }
     return length;
